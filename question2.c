39e8292af4698beb4f793d89a74158f802234be4
Hannah Roeder <hannah.roeder@tcu.edu>
2025-12-03 12:40:37 -0600

    Changed the program to not use logical operators.

--- question2.c
+++ question2.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int max3(int a, int b, int c) {
-        if (a > b && a > c){
-                return a;
-        }
-        else if (b > a && b > c) {
-                return b;
-        }
-        else if (c > b && c > a) {
-                return c;
-        }
-        else {
-
-        }
+	int max = a;  
+    	if (b > max) {
+        	max = b;
+    	}
+    	if (c > max) {
+        	max = c;
+    	}
+   	 return max;
 }
+
 int main() {
         int num1, num2, num3;
         int largest;
