6831d6e93c36423a3c29a1965dfb75aeb0457b24
Hannah Roeder <hannah.roeder@tcu.edu>
2025-12-03 17:16:18 -0600

    Created a program to reverse an array

---
+++ question9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int arr[5];
+
+	printf("Enter 5 integers:\n");
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < 5 / 2; i++) {
+		int temp = arr[i];
+		arr[i] = arr[5 - 1 - i];
+		arr[5 - 1 - i] = temp;
+	}
+
+	printf("Reversed array: ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
