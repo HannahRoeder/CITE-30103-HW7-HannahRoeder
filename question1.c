6b7589193285f9c678fbca76e2ae9fa8727c8898
Hannah Roeder <hannah.roeder@tcu.edu>
2025-12-03 12:27:37 -0600

    Wrote a program to convert fahrenheit to celsius using a function.

---
+++ question1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float toCelsius(float f) {
+        return (f - 32) * 5.0/9.0;
+}
+
+int main() {
+        float celsius, fahrenheit;
+        printf("Enter fahrenheit: ");
+        scanf("%f", &fahrenheit);
+
+        celsius = toCelsius(fahrenheit);
+
+        printf("Celsius: %.2f\n", celsius);
+
+        return 0;
+}
+
