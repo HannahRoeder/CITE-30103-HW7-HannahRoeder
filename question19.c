22da2b8e73f2298be37c15e02ef8d2082590e53f
Hannah Roeder <hannah.roeder@tcu.edu>
2025-12-03 22:31:26 -0600

    Created a program to include a DOB feature as a student field.

---
+++ question19.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct Date {
+	int day;
+	int month;
+	int year;
+};
+
+struct Student {
+	char name[40];
+	int roll;
+	float marks;
+	struct Date dob;
+};
+
+int main() {
+	struct Student s;
+
+	printf("Name: ");
+	scanf("%39s", s.name);
+
+	printf("Roll: ");
+	scanf("%d", &s.roll);
+
+	printf("Marks: ");
+	scanf("%f", &s.marks);
+
+	printf("DOB (day month year): ");
+	scanf("%d %d %d", &s.dob.day, &s.dob.month, &s.dob.year);
+
+	printf("Name: %s\n", s.name);
+	printf("Roll: %d\n", s.roll);
+	printf("Marks: %.2f\n", s.marks);
+	printf("Date of birth: %02d-%02d-%04d\n", s.dob.day, s.dob.month, s.dob.year);
+
+	return 0;
+}
