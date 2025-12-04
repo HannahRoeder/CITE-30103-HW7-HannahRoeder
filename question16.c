2afa06f5a5ee0206b57b033cb7979fed4be9c336
Hannah Roeder <hannah.roeder@tcu.edu>
2025-12-03 21:39:36 -0600

    Created a program create a struct student and display the data

---
+++ question16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student {
+	char name[40];
+	int roll;
+	float marks;
+};
+
+int main() {
+	struct Student s;
+
+	printf("Enter name: ");
+	scanf("%39s", s.name);
+
+	printf("Enter roll num: ");
+	scanf("%d", &s.roll);
+
+	printf("Enter marks: ");
+	scanf("%f", &s.marks);
+
+	printf("Name: %s\n", s.name);
+	printf("Roll: %d\n", s.roll);
+	printf("Marks: %.2f\n", s.marks);
+
+	return 0;	
+}
